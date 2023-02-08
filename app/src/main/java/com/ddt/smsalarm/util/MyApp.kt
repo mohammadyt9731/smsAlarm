c7fc78c86d16cdc113ca1a1cdd608ea8bfb35fd9
Mohammad Yazdi <mohammadyt1379@gmail.com>
2023-02-08 22:14:11 +0330

    -set to lightModel

--- app/src/main/java/com/ddt/smsalarm/util/MyApp.kt
+++ app/src/main/java/com/ddt/smsalarm/util/MyApp.kt
@@ -1,8 +1,15 @@
 package com.ddt.smsalarm.util
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApp :Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        //disable night mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
